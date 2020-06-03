d9f6a182b8a97db93d0264f535ab6eed139a1c4f
Dr Z <zamore@vt.edu>
2020-06-02 18:03:21 -0600

    Turtle start added

---
+++ Turtle_start.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+'''
+Turtle starter code
+ATLS 1300
+Author: Dr. Z
+May 29, 2020
+'''
+
+from turtle import * #import the library of commands that you'd like to use
+
+#Create a panel to draw on. 
+panel = Screen()
+w = 600 # width of panel
+h = 600 # height of panel
+panel.setup(width=w, height=h) #600 x 600 is a decent size to work on. 
+#You can experiment by making it the size of your screen or super tiny!
+
+#Don't change this line (puts (0,0) at upper left corner)
+screen.setworldcoordinates(0, w, h, 0)
+
+#=======Add your code here======
+
+
+# In[ ]:
+
+
+
+
