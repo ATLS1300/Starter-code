41f79d661adc726d0ba4ca89261e4bf41dd50891
Dr Z <zamore@vt.edu>
2020-06-03 15:15:54 -0600

    fixed screen error

--- Turtle_start.py
+++ Turtle_start.py
@@ -5,6 +5,7 @@
 Turtle starter code
 ATLS 1300
 Author: Dr. Z
+Author: YOUR NAME
 May 29, 2020
 '''
 
@@ -19,10 +20,30 @@ panel.setup(width=w, height=h) #600 x 600 is a decent size to work on.
 #You can experiment by making it the size of your screen or super tiny!
 
 #Don't change this line (puts (0,0) at upper left corner)
-screen.setworldcoordinates(0, w, h, 0)
+panel.setworldcoordinates(0, w, h, 0)
 
 #=======Add your code here======
 
+window = Screen()
+window.clear()
+window.bgcolor("orange")
 
+Harry = Turtle()
+Harry.shape("circle")
+Harry.forward(50)
+Harry.backward(100) #pixels
+Harry.left(90) #degrees
+Harry.forward(100)
+Harry.right(30)
+Harry.backward(150)
 
+Harry.goto(0,0)
+Harry.goto(100,100)
 
+Harry.up() 
+Harry.goto(200,200)
+Harry.down()
+Harry.forward(200)
+
+Harry.circle(50) #radius
+Harry.dot(50) #diameter
